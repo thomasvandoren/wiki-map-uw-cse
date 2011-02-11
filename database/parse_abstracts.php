85c04e69538ecb4a3164b75c3d6807cda3f06133
Rob McClure <mgracecubs@gmail.com>
2011-02-11 15:10:43 -0800

    Changed WikiInitialization.sql to transform.sql to clarify purpose Changed parse_abstracts to generate a SQL script to create and load the abstract table, which resulted in better performance Created a Makefile to fully initialize a database for use by WikiGraph

--- database/parse_abstracts.php
+++ database/parse_abstracts.php
@@ -1,12 +1,48 @@
 <?php
-include "config.php";
+/*
+WikiGraph
+Copyright (c) 2011
+
+Author: Rob McClure <mgracecubs@gmail.com>
+
+Abstract Parser
+Takes the abstract XML dump from a MediaWiki database (abstract.xml),
+parses it to find the titles and abstracts,
+and then creates a SQL script to create and fill the
+abstract table.
+Script relies on the page table that results from
+transform.sql
+*/
+
+include 'config.php';
+
+mysql_connect($host, $user, $pass);
+mysql_select_db($db);
+
+
+$sqlfile = "abstract.sql";
+$fh = fopen($sqlfile, 'w');
+
+$sql = <<<STRING
+DROP TABLE IF EXISTS `abstract`;
+
+CREATE TABLE `abstract` (
+       `abstract_id` int(8),
+       `abstract_title` varbinary(255),
+       `abstract_text` text
+) ENGINE=InnoDB DEFAULT CHARSET=binary;
+
+STRING;
+
+fwrite($fh, $sql);
+
+
 
 $reader = new XMLReader();
-$reader->open("enwikibooks-20110127-abstract.xml");
+$reader->open("abstract.xml");
 
-$db = mysql_connect("localhost", $username, $password);
-mysql_select_db($database);
-
+$count = 0;
+$pairs = array();
 while ($reader->read()) {
   if ($reader->name == "doc" && $reader->nodeType == XMLReader::ELEMENT) {
     // Found a page
@@ -15,15 +51,13 @@ while ($reader->read()) {
     while ($reader->read()) {
       if ($reader->name == "doc" && $reader->nodeType == XMLReader::END_ELEMENT) {
 	// Page is done, get information and insert into database
-
-	// Find page id for this title
-	$query = "SELECT page_id FROM page WHERE page_title = '" . mysql_real_escape_string($title) . "' AND page_namespace = 0";
-	$results = mysql_query($query);
-	$row = mysql_fetch_array($results);
-	if ($row) {
-	  // Insert into database if this is a valid entry
-	  $page_id = $row["page_id"];
-	  mysql_query("INSERT INTO abstract (`abstract_id`, `abstract_text`) VALUES (" . $page_id . ", '" . mysql_real_escape_string($abstract) . "')");
+	$pairs[$count] = "(NULL, '" . $title . "','" . $abstract . "')";
+	$count++;
+	if ($count == 1000) {
+	  $str = "INSERT INTO `abstract` VALUES" . implode(",", $pairs) . ";\n";
+	  fwrite($fh, $str);
+	  $count = 0;
+	  $pairs = array();
 	}
 	break;
       } else if ($reader->name == "title" && $reader->nodeType == XMLReader::ELEMENT) {
@@ -34,15 +68,26 @@ while ($reader->read()) {
 	$title = substr($reader->value, 11);
 	// and replace spaces with _
 	$title = str_replace(" ", "_", $title);
+	$title = mysql_real_escape_string($title);
       } else if ($reader->name == "abstract" && $reader->nodeType == XMLReader::ELEMENT) {
 	// Get the article abstract
 	$reader->read();
 	$abstract = $reader->value;
+	$abstract = mysql_real_escape_string($abstract);
       }
     }
   }
 }
 
+$str = "INSERT INTO `abstract` VALUES" . implode(",", $pairs) . ";\n";
+fwrite($fh, $str);
+
 $reader->close();
+
+// This step takes many minutes. Is there a way to speed it up?
+fwrite($fh, "UPDATE abstract SET abstract_id=(SELECT page_id FROM page WHERE page_title = abstract_title LIMIT 1);\n");
+fwrite($fh, "DELETE FROM abstract WHERE abstract_id IS NULL;");
+fwrite($fh, "ALTER TABLE abstract DROP COLUMN abstract_title, CHANGE `abstract_id` `abstract_id` INT(8) UNSIGNED NOT NULL PRIMARY KEY\n");
+fclose($fh);
 
 ?>
