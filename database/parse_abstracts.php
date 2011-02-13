685abbf2bc67f2cedba465f014535a525333284e
Rob McClure <mgracecubs@gmail.com>
2011-02-13 15:38:20 -0800

    Changed parse_abstracts to allow for multiple XML files, which was necessary because the abstract.xml for wikipedia was too large Changed Makefile to provide the links to multiple XML files Removed the dummy config file because it is now produced by the Makefile

--- database/parse_abstracts.php
+++ database/parse_abstracts.php
@@ -12,12 +12,14 @@ and then creates a SQL script to create and fill the
 abstract table.
 Script relies on the page table that results from
 transform.sql
+Script relies on the paths to the XML files in abstract_tmp
+These paths can be URLs
 */
 
 include 'config.php';
 
 mysql_connect($host, $user, $pass);
-mysql_select_db($db);
+mysql_select_db($dbname);
 
 
 $sqlfile = "abstract.sql";
@@ -37,54 +39,61 @@ STRING;
 fwrite($fh, $sql);
 
 
+$files = explode("\n", chop(file_get_contents("abstract_tmp")));
 
-$reader = new XMLReader();
-$reader->open("abstract.xml");
+foreach ($files as $i => $file) {
+  print("Parsing $file\n");
+  fwrite($fh, "SOURCE abstract$i.sql;\n");
+  $fhp = fopen("abstract$i.sql", 'w');
 
-$count = 0;
-$pairs = array();
-while ($reader->read()) {
-  if ($reader->name == "doc" && $reader->nodeType == XMLReader::ELEMENT) {
-    // Found a page
-    $title = null;
-    $abstract = null;
-    while ($reader->read()) {
-      if ($reader->name == "doc" && $reader->nodeType == XMLReader::END_ELEMENT) {
-	// Page is done, get information and insert into database
-	$pairs[$count] = "(NULL, '" . $title . "','" . $abstract . "')";
-	$count++;
-	if ($count == 1000) {
-	  $str = "INSERT INTO `abstract` VALUES" . implode(",", $pairs) . ";\n";
-	  fwrite($fh, $str);
-	  $count = 0;
-	  $pairs = array();
+  $reader = new XMLReader();
+  $reader->open($file);
+
+  $count = 0;
+  $pairs = array();
+  while ($reader->read()) {
+    if ($reader->name == "doc" && $reader->nodeType == XMLReader::ELEMENT) {
+      // Found a page
+      $title = null;
+      $abstract = null;
+      while ($reader->read()) {
+	if ($reader->name == "doc" && $reader->nodeType == XMLReader::END_ELEMENT) {
+	  // Page is done, get information and insert into database
+	  $pairs[$count] = "(NULL, '" . $title . "','" . $abstract . "')";
+	  $count++;
+	  if ($count == 1000) {
+	    $str = "INSERT INTO `abstract` VALUES" . implode(",", $pairs) . ";\n";
+	    fwrite($fhp, $str);
+	    $count = 0;
+	    $pairs = array();
+	  }
+	  break;
+	} else if ($reader->name == "title" && $reader->nodeType == XMLReader::ELEMENT) {
+	  // Get the article title
+
+	  $reader->read();
+	  // All titles begin with Wikibooks: , which we need to get rid of
+	  $title = substr($reader->value, 11);
+	  // and replace spaces with _
+	  $title = str_replace(" ", "_", $title);
+	  $title = mysql_real_escape_string($title);
+	} else if ($reader->name == "abstract" && $reader->nodeType == XMLReader::ELEMENT) {
+	  // Get the article abstract
+	  $reader->read();
+	  $abstract = $reader->value;
+	  $abstract = mysql_real_escape_string($abstract);
 	}
-	break;
-      } else if ($reader->name == "title" && $reader->nodeType == XMLReader::ELEMENT) {
-	// Get the article title
-
-	$reader->read();
-	// All titles begin with Wikibooks: , which we need to get rid of
-	$title = substr($reader->value, 11);
-	// and replace spaces with _
-	$title = str_replace(" ", "_", $title);
-	$title = mysql_real_escape_string($title);
-      } else if ($reader->name == "abstract" && $reader->nodeType == XMLReader::ELEMENT) {
-	// Get the article abstract
-	$reader->read();
-	$abstract = $reader->value;
-	$abstract = mysql_real_escape_string($abstract);
       }
     }
   }
+
+  $str = "INSERT INTO `abstract` VALUES" . implode(",", $pairs) . ";\n";
+  fwrite($fhp, $str);
+
+  $reader->close();
+  fclose($fhp);
 }
 
-$str = "INSERT INTO `abstract` VALUES" . implode(",", $pairs) . ";\n";
-fwrite($fh, $str);
-
-$reader->close();
-
-// This step takes many minutes. Is there a way to speed it up?
 fwrite($fh, "UPDATE abstract SET abstract_id=(SELECT page_id FROM page WHERE page_title = abstract_title LIMIT 1);\n");
 fwrite($fh, "DELETE FROM abstract WHERE abstract_id IS NULL;");
 fwrite($fh, "ALTER TABLE abstract DROP COLUMN abstract_title, CHANGE `abstract_id` `abstract_id` INT(8) UNSIGNED NOT NULL PRIMARY KEY\n");
