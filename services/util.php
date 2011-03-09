c047992769e30f32d4860364b5b5c97edd04d178
Jeremy Lenz <no.jeremy1212@gmail.com>
2011-03-09 13:08:51 -0800

    changed soundex query in util.php to not include like query matches

--- services/util.php
+++ services/util.php
@@ -177,8 +177,7 @@ LIMIT 150";
    * Takes the string to search for
    */
   public function get_search_results_like($like) {
-    $q =  "SELECT page_id, page_title FROM page WHERE page_title LIKE 
-'$like%' LIMIT 150";
+    $q =  "SELECT page_id, page_title FROM page WHERE page_title LIKE '$like%' LIMIT 150";
     return $this->query($q);
   }
 
@@ -188,7 +187,8 @@ LIMIT 150";
    */
   public function get_search_results_soundex($like) {
     $q =  "SELECT page_id, page_title FROM page WHERE page_title_soundex = 
-LEFT(SOUNDEX('$like'),4) LIMIT 150";
+LEFT(SOUNDEX('$like'),4) AND NOT IN (SELECT page_id, page_title FROM page WHERE 
+page_title LIKE '$like%' LIMIT 150) LIMIT 150";
     return $this->query($q);
   }
 
